bb533974b24020c4a160ee14e5a00216f437b4b5
uguryldz <uguryldz1994@gmail.com>
2025-10-28 07:08:33 +0000

    lead2ok

--- creqit/meta/doctype/facebook_lead_ads/facebook_lead_ads.py
+++ creqit/meta/doctype/facebook_lead_ads/facebook_lead_ads.py
@@ -4,6 +4,8 @@
 import creqit
 from creqit import _
 from creqit.model.document import Document
+import json
+from datetime import datetime
 
 
 class FacebookLeadAds(Document):
@@ -11,6 +13,17 @@ class FacebookLeadAds(Document):
 	
 	def before_save(self):
 		"""Set timestamps before saving"""
+		# Parse created_time from lead_data if exists
+		if self.lead_data and not self.created_at:
+			try:
+				lead_data_dict = json.loads(self.lead_data) if isinstance(self.lead_data, str) else self.lead_data
+				if 'created_time' in lead_data_dict:
+					timestamp = int(lead_data_dict['created_time'])
+					dt = datetime.fromtimestamp(timestamp)
+					self.created_at = dt.strftime('%d.%m.%Y %H:%M')
+			except (json.JSONDecodeError, ValueError, KeyError):
+				pass
+		
 		if not self.created_at:
 			self.created_at = creqit.utils.now()
 		self.updated_at = creqit.utils.now()
